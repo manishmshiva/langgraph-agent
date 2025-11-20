1db7c218fd1d507c38a99f33752454c92804935c
root <root@DESKTOP-OBFG8E6.>
2025-11-20 06:02:06 +0000

    reasoning with function calling

--- react.py
+++ react.py
@@ -1,1 +1,19 @@
 # Reasoning engine
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+@tool
+def tripleFloat(num:float) -> float:
+    """ Triple the input float number and return the result """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1),tripleFloat]
+
+llm = ChatOpenAI(model="gpt-4o-mini",temperature=0).bind_tools(tools)
+
+
