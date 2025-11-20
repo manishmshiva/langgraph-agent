9599d4caf33e55ba661025799bf8de074b853e0d
Manish Shivanandhan <manish@manalath.com>
2025-11-20 06:37:53 +0000

    nodes

---
+++ nodes.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langgraph.graph import MessagesState
+from langgraph.prebulit import ToolNode
+
+from react import tools, llm
+
+SYSTEM_MESSAGE = """
+You are a helpful assistant that can use tools to answer questions
+"""
+
+def agent_reasoning_node(state:MessagesState) -> MessagesState:
+    """
+    Run the agent reasoning node
+    """
+    response = llm.invoke([{"role":"system","content":SYSTEM_MESSAGE},*state["messages"]])
+    return {"messages":[response]}
+
+tool_node = ToolNode(tools)
